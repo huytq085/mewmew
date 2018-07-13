be4e4dd2f0ea175885f69ee69df53e2df3fd8a29
tranquanghuy341 <tranquanghuy341@gmail.com>
2018-07-13 21:57:30 +0700

    generate components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material/app-material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
 
+import { HeaderComponent } from './shared/layout/header.component'
+import { FooterComponent } from './shared/layout/footer.component'
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
+import { AuthModule } from './auth/auth.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent, FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
