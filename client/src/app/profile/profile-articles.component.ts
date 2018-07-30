ec31cae45b48cf114ceea8d1ea22b6b02e288c48
huytq085 <=>
2018-07-30 15:54:19 +0700

    routerLinkActive

--- client/src/app/profile/profile-articles.component.ts
+++ client/src/app/profile/profile-articles.component.ts
@@ -13,21 +13,12 @@ export class ProfileArticlesComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private userService: UserService
-  ) { 
+  ) {
     // Reload when switching user profile
     // override the route reuse strategy
-    this.router.routeReuseStrategy.shouldReuseRoute = function(){
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
       return false;
-   }
-
-   this.router.events.subscribe((evt) => {
-      if (evt instanceof NavigationEnd) {
-         // trick the Router into believing it's last link wasn't previously loaded
-         this.router.navigated = false;
-         // if you need to scroll back to top, here is the right place
-         window.scrollTo(0, 0);
-      }
-  });
+    }
   }
 
   user: User;
